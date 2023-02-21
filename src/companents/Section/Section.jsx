7d4d1fc654d47a9cf44cb09cee025ef04045c266
QodirovDoston <qodirovdoston806@gmail.com>
2023-02-21 15:10:25 +0500

    ddfdf

--- src/companents/Section/Section.jsx
+++ src/companents/Section/Section.jsx
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from 'react'
 import "../Section/Section.css"
 import axios from 'axios'
 import { Col, Container, Form, Row } from 'react-bootstrap'
-const homeCard = require('./img/home.png')
 
 function Section() {
     const [today, setToday] = useState([])
     const [value, SetValue] = useState("")
 
     const FetchingData = async () => {
-        const res = await axios.get("http://localhost:3000/data")
+        const res = await axios.get("http://localhost:3002/data")
         return res
     }
     useEffect(() => {
@@ -19,37 +18,36 @@ function Section() {
         <div>
             <Container>
                 <Row className='justify-content-center mt-5'>
-                        <Form.Group className="dilm mb-3" controlId="formBasicPassword">
-                            <Form.Control type="text" onChange={(e)=>SetValue(e.target.value)} />
-                        </Form.Group>
-                        <h1 className='felter'>Лучшие предложения рядом с вами</h1>
+                    <Form.Group className="dilm mb-3" controlId="formBasicPassword">
+                        <Form.Control type="text" onChange={(e) => SetValue(e.target.value)} />
+                    </Form.Group>
+                    <h1 className='felter'>Лучшие предложения рядом с вами</h1>
                 </Row>
                 <Row>
+                    {today.filter((res) => {
+                        console.log(res.name);
+                        return res.name.toLocaleLowerCase() === "" ? value : res.name.toLocaleLowerCase().includes(value)
+                    }).map((val) => {
+                        return (
+                            <Col xs={3}>
+                                <div className="card" key={val.id}>
+                                    <img src={val.img} alt="" />
+                                    <h2 className='solo'>{val.name}</h2>
+                                    <p className='soso'><i class="fa-thin fa-location-dot"></i>{val.location}</p>
 
-                {today.filter((res) =>{
-                    console.log(res.name);
-                    return res.name.toLocaleLowerCase() === "" ? value : res.name.toLocaleLowerCase().includes(value)
-                }).map((val) => {
-                    return (
-                    <Col xs={3}>
-                    <div className="card" key={val.id}>
-                        <img src={val.img} alt="" />
-                        <h2 className='solo'>{val.name}</h2>
-                        <p  className='soso'><i class="fa-thin fa-location-dot"></i>{val.location}</p>
-                       
-                        <div className='bothbuton'>
-                            <button className='byu'>{val.price}</button>
-                            <button className='bre'>{val.sale}</button>
-                        </div>
-                    </div>
-                    <div className="newcard">
-                        <h1 className='newcard0'>8 990 ₽  / за ночь</h1>
-                        <p className='newcard1'>12 990  </p>
-                    </div>
-                    </Col>
-                                )
-                            })
-                         } 
+                                    <div className='bothbuton'>
+                                        <button className='byu'>{val.price}</button>
+                                        <button className='bre'>{val.sale}</button>
+                                    </div>
+                                </div>
+                                <div className="newcard">
+                                    <h1 className='newcard0'>8 990 ₽  / за ночь</h1>
+                                    <p className='newcard1'>12 990  </p>
+                                </div>
+                            </Col>
+                        )
+                    })
+                    }
                 </Row>
             </Container>
         </div>
